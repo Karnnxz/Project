e3053f7a3f813eb663ffd06738a1bdc8ed4a390a
Wimwipa780 <wimwipha4746@gmail.com>
2025-02-24 13:55:01 +0700

    Update Coin.cpp

--- Coin.cpp
+++ Coin.cpp
@@ -1,16 +1,39 @@
+#include <cmath> // Add this line to include the cmath header
 #include "Coin.h"
 
-Coin CreateCoin(float x, float y) {
-    Coin coin = { {x, y, 20, 20}, false };
-    return coin;
-}
+Coin::Coin(float x, float y) : rec{ x, y, 20, 20 }, collected(false), initialY(y), oscillationSpeed(2.0f), oscillationAmplitude(10.0f) {} // Increase oscillationAmplitude to 10.0f
 
-void UpdateCoin(Coin* coin) {
-    if (coin->collected) return;
-}
-
-void DrawCoin(Coin coin) {
-    if (!coin.collected) {
-        DrawCircleV(Vector2{ coin.rec.x + coin.rec.width / 2, coin.rec.y + coin.rec.height / 2 }, 10, YELLOW);
+void Coin::Update() {
+    if (!collected) {
+        // Oscillation logic
+        rec.y = initialY + oscillationAmplitude * sin(GetTime() * oscillationSpeed);
     }
+}
+
+void Coin::Draw() {
+    if (!collected) {
+        DrawCircle(rec.x + rec.width / 2, rec.y + rec.height / 2, rec.width / 2, GOLD);
+    }
+}
+
+bool Coin::IsCollected() const {
+    return collected;
+}
+
+void Coin::Collect() {
+    collected = true;
+}
+
+Rectangle Coin::GetRec() const {
+    return rec;
+}
+
+void Coin::Reset() {
+    collected = false;
+    rec.y = initialY; // Reset the y-position to the initial value
+}
+
+void Coin::SetPosition(float x, float y) { // Add this method
+    rec.x = x;
+    rec.y = y;
 }
